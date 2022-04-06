7107081d99261d4f531ea113e19f4bacf308c1f5
pritamb12 <pritam.b@comakeit.com>
2022-04-06 12:27:49 +0530

    backend

---
+++ Software_chasers_Backend/src/main/java/com/Software_chasers_Backend/springboot/SoftwareChasersBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Software_chasers_Backend.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareChasersBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareChasersBackendApplication.class, args);
+	}
+
+}
